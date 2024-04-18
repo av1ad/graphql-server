4578df14699be2f4b677183ff6f39c6f7d610cec
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-18 15:00:52 -0400

    Finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 
 const app: Application = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers});
 
 async function startServer() {
   await server.start();
